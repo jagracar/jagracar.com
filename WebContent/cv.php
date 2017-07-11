93587bd1eb900a68fc337134a944f1283f6cdc88
Javier Gracia Carpio <jagracar@gmail.com>
2017-07-12 01:34:27 +0200

    Corrects jquery link to use https

--- WebContent/cv.php
+++ WebContent/cv.php
@@ -244,7 +244,7 @@
 	<?php include_once $homeDir . 'footer.php';?>
 
 	<!-- JavaScript files -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="<?php echo $homeDir; ?>js/cv.js"></script>
 </body>
 </html>
